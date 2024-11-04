dbe8c9235697aad6ae0376f016898fb6be91a1c0
Josh Karpel <josh.karpel@gmail.com>
2024-11-04 00:46:02 -0600

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,57 @@
-fn main() {
-    println!("Hello, world!");
+mod state;
+
+use std::io;
+
+use ratatui::{
+    crossterm::event::{self, KeyCode, KeyEvent, KeyEventKind},
+    layout::{
+        Constraint::{Length, Min, Ratio},
+        Layout,
+    },
+    widgets::{Block, Paragraph},
+    DefaultTerminal,
+};
+
+use crate::state::State;
+
+fn main() -> io::Result<()> {
+    let mut terminal = ratatui::init();
+    terminal.clear()?;
+    let app_result = run(terminal);
+    ratatui::restore();
+    app_result
+}
+
+fn run(mut terminal: DefaultTerminal) -> io::Result<()> {
+    let state = State::default();
+
+    loop {
+        let tab = state.active_tab();
+
+        terminal.draw(|frame| {
+            let [meta, resources] = Layout::vertical([Length(3), Min(0)]).areas(frame.area());
+            let [namespace, resource, filter] =
+                Layout::horizontal([Ratio(1, 3), Ratio(1, 3), Ratio(1, 3)]).areas(meta);
+
+            let namespace_p = Paragraph::new(tab.namespace.clone().unwrap_or("".into()))
+                .block(Block::bordered().title("Namespace"));
+            let resource_p =
+                Paragraph::new(tab.resource.clone()).block(Block::bordered().title("Resource"));
+            let filter_p =
+                Paragraph::new(tab.filter.clone()).block(Block::bordered().title("Filter"));
+
+            frame.render_widget(namespace_p, namespace);
+            frame.render_widget(resource_p, resource);
+            frame.render_widget(filter_p, filter);
+        })?;
+
+        if let event::Event::Key(KeyEvent {
+            kind: KeyEventKind::Press,
+            code: KeyCode::Char('q'),
+            ..
+        }) = event::read()?
+        {
+            return Ok(());
+        }
+    }
 }
---
+++ src/state.rs
@@ -0,0 +1,39 @@
+#[allow(dead_code)]
+#[derive(Debug)]
+pub(crate) struct State {
+    tabs: Vec<Tab>,
+    active_tab_idx: usize,
+}
+
+impl Default for State {
+    fn default() -> Self {
+        Self {
+            tabs: vec![Tab::default()],
+            active_tab_idx: 0,
+        }
+    }
+}
+
+impl State {
+    pub fn active_tab(&self) -> &Tab {
+        &self.tabs[self.active_tab_idx]
+    }
+}
+
+#[allow(dead_code)]
+#[derive(Debug)]
+pub(crate) struct Tab {
+    pub(crate) namespace: Option<String>,
+    pub(crate) resource: String,
+    pub(crate) filter: String,
+}
+
+impl Default for Tab {
+    fn default() -> Self {
+        Self {
+            namespace: None,
+            resource: "pods".to_string(),
+            filter: String::default(),
+        }
+    }
+}
